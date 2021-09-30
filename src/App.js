b0c21ab932cf809d570d49748ba194fcf5e934b8
Fernanda <78928436+FernandaNalon@users.noreply.github.com>
2021-09-30 09:58:43 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React from 'react';
+
+
+   var a = parseInt(prompt("Digite um número: "));
+   var b = parseInt(prompt("Digite um valor: "));
+
+function App () {
+   
+
+   function teste() {
+      return (a+b);
+   }
+
+   return (
+      <div className = "App">
+         <p>A soma desses valores digitados são: {teste()}</p>
+      </div>
+   );
+}
+export default App;
